67e6b8c909f1f00736eaf31b8685adca52124459
Wilson <1092530289@qq.com>
2019-07-23 16:11:01 +0800

    修改测试

--- springboot-demo/src/main/java/com/sun/demo/SampleApplication.java
+++ springboot-demo/src/main/java/com/sun/demo/SampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 
  * @author wilson
- *
+ * @author wilson
  */
 @SpringBootApplication
 public class SampleApplication {
