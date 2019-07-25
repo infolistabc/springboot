e5b8a3a94ffeff6ed5c1135aae2f99932f01821a
Wilson <1092530289@qq.com>
2019-07-25 17:06:59 +0800

    简单应用

--- springboot-demo/src/main/java/com/sun/demo/SampleApplication.java
+++ springboot-demo/src/main/java/com/sun/demo/SampleApplication.java
@@ -3,8 +3,6 @@ package com.sun.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
- * 
- * @author wilson
  * @author wilson
  */
 @SpringBootApplication
