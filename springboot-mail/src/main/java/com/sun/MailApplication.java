af42888b1f432117da501e8c1c7382f9feb45404
Wilson <1092530289@qq.com>
2019-08-08 18:45:22 +0800

    整合邮件发送

--- springboot-mail/src/main/java/com/sun/MailApplication.java
+++ springboot-mail/src/main/java/com/sun/MailApplication.java
@@ -3,7 +3,10 @@ package com.sun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class MailApplication {
 
 	public static void main(String[] args) {
