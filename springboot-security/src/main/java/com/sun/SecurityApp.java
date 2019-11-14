daa002ff756dae9b11003d814491e29c2872aa90
Wilson <1092530289@qq.com>
2019-11-14 22:46:37 +0800

    spring security

---
+++ springboot-security/src/main/java/com/sun/SecurityApp.java
@@ -0,0 +1,18 @@
+package com.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author wilson
+ *
+ */
+@SpringBootApplication
+public class SecurityApp 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SecurityApp.class, args);
+    }
+}
