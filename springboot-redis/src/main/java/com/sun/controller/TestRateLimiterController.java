1b9aa2d23b4890e0116887611f127dd5868b83ac
Wilson <1092530289@qq.com>
2020-12-30 17:13:50 +0800

    test redis-cell

--- springboot-redis/src/main/java/com/sun/controller/TestRateLimiterController.java
+++ springboot-redis/src/main/java/com/sun/controller/TestRateLimiterController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.TimeUnit;
@@ -18,24 +19,46 @@ public class TestRateLimiterController {
     SimpleRateLimiterUtil simpleRateLimiterUtil;
 
     @GetMapping("/setNxRate")
-    public String setNxRate(HttpServletRequest request){
-       boolean flag = simpleRateLimiterUtil.setNxRate("action:url:"+request.getRequestURI(),2,10,TimeUnit.SECONDS);
-        if (flag){
+    public String setNxRate(HttpServletRequest request) {
+        boolean flag = simpleRateLimiterUtil.setNxRate("action:url:" + request.getRequestURI(), 2, 10, TimeUnit.SECONDS);
+        if (flag) {
             return "正常访问";
-        }else {
+        } else {
             return "频发访问";
         }
     }
 
     @GetMapping("/setLimitFlow")
-    public String setLimitFlow(){
+    public String setLimitFlow() {
         String key = "setLimitFlow";
-        boolean flag = simpleRateLimiterUtil.setLimitFlow(key,60L);
-        if (flag){
+        boolean flag = simpleRateLimiterUtil.setLimitFlow(key, 60L);
+        if (flag) {
             return "正常访问";
-        }else {
+        } else {
             return "频发访问";
         }
+    }
 
+    @GetMapping("/redisCell")
+    public String redisCell() {
+        // 接口API
+        String key = "/user/list";
+        // 漏斗初始容量
+        int capacity = 5;
+        // 单位时间内允许访问的频率
+        int rate = 8;
+        // 单位时间(秒)，即每秒允许有8次访问
+        int seconds = 1;
+        // 每次请求漏斗容量减少的数量
+        int tocken_num = 1;
+        for (int i = 0; i < 10; i++) {
+            boolean allow = simpleRateLimiterUtil.throttle(key, capacity, rate, seconds, tocken_num);
+            if (allow) {
+                System.out.println("正常执行请求:" + i);
+            } else {
+                System.out.println("被限流了:" + i);
+            }
+        }
+        return "";
     }
 }
--- springboot-redis/src/main/java/com/sun/util/SimpleRateLimiterUtil.java
+++ springboot-redis/src/main/java/com/sun/util/SimpleRateLimiterUtil.java
@@ -1,9 +1,12 @@
 package com.sun.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -12,6 +15,7 @@ import java.util.concurrent.TimeUnit;
  * 简单的限流工具类
  */
 @Component
+@Slf4j
 public class SimpleRateLimiterUtil {
     @Resource
     private RedisTemplate<String,Object> redisTemplate;
@@ -47,5 +51,35 @@ public class SimpleRateLimiterUtil {
             }
         }
         return redisTemplate.opsForZSet().add(key, UUID.randomUUID().toString(),currentTime);
+    }
+
+    /**
+     *
+     * @param key      接口名称
+     * @param capacity 漏斗初始容量
+     * @param rate     单位时间内访问频率/秒
+     * @param seconds  单位时间(秒)
+     * @param tokenNum 每次请求漏斗容量减少的数量
+     * @return
+     */
+    public boolean throttle(String key, int capacity, int rate, int seconds, int tokenNum){
+        try {
+            DefaultRedisScript script = new DefaultRedisScript();
+            script.setResultType(Long.class);
+            /*
+                cl.throttle命令返回的是一个table，这里只关注第一个元素0表示正常，1表示过载
+                KEYS[1]需要设置的key值，结合业务需要可以是接口名称+用户ID+购买的资源包等等等等
+                ARGV[1]漏斗初始容量
+                ARGV[2]频率次数，结合ARGV[3]一起使用
+                ARGV[3]周期（秒），结合ARGV[2]一起使用
+                ARGV[4]每次请求漏斗容量减少的数量,默认为1
+             */
+            script.setScriptText("return redis.call('cl.throttle',KEYS[1], ARGV[1], ARGV[2], ARGV[3], ARGV[4])[1]");
+            Long rst = (Long) redisTemplate.execute(script, Arrays.asList(key), capacity, rate, seconds, tokenNum);
+            return rst == 0;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return false;
+        }
     }
 }
