75ae4942d7c2990ec76149ac9beec51612bf1ece
wilson <qiansl@hua-cloud.com.cn>
2021-04-14 19:28:01 +0800

    feat(提交user_vo.proto文件): springboot集成grpc远程调用

---
+++ grpc-test/grpc-lib/src/main/java/com/sun/lib/grpc/TestClass.java
@@ -0,0 +1,18 @@
+package com.sun.lib.grpc;
+
+import com.sun.lib.grpc.vo.AddUser;
+
+public class TestClass {
+    public static void main(String[] args) {
+        //创建一个对象
+        AddUser.Builder addUser = AddUser.newBuilder();
+        addUser.setName("张三");
+        addUser.setAge(12);
+        addUser.addPhone("11111");
+        addUser.addPhone("22222");
+        addUser.setAddress("测试地址");
+        addUser.setSex(AddUser.Sex.MALE);
+        addUser.build();
+        System.out.printf("输出\n"+addUser.build());
+    }
+}
